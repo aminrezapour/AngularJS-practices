ae7cf4be8bdaca28fa72181a288ec419186a6635
Amin Rezapour <aminrezapour@gmail.com>
2016-03-18 11:31:17 -0700

    Add basics of Angular components: controller, module, directive and expression

--- gemstore/app.js
+++ gemstore/app.js
@@ -1,1 +1,23 @@
-var app = angular.module('gemStore', []);
+(function(){
+  var app = angular.module('gemStore', []);
+
+  app.controller('StoreController', function(){
+     this.products = gem;
+   });
+
+   var gem =[
+     {
+       name: 'Azurite',
+       price: 2.95,
+       canPurchase: false,
+       soldOut: false
+     },
+     {
+       name: 'Sapphire',
+       price: 95,
+       canPurchase: true,
+       soldOut: false
+     }
+   ];
+
+})();
