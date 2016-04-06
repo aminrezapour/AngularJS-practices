8d227cde6a24ad5757cf80b4ae435bee0edb4605
Amin Rezapour <aminrezapour@gmail.com>
2016-04-05 23:24:15 -0700

    Add routes

--- Reddit/app.js
+++ Reddit/app.js
@@ -1,4 +1,18 @@
-angular.module("angularReddit", [])
+angular.module("angularReddit", ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+  
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}])
 .factory('posts', [
 function(){
   var o = {
