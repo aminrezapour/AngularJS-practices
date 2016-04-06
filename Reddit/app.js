fd44fbeb0fad66959ea66a2b3fdee707fa87cf45
Amin Rezapour <aminrezapour@gmail.com>
2016-04-05 20:33:38 -0700

    Add service

--- Reddit/app.js
+++ Reddit/app.js
@@ -1,15 +1,22 @@
 angular.module("angularReddit", [])
+.factory('posts', [
+function(){
+  var o = {
+    posts: [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 7},
+      {title: 'post 4', upvotes: 4},
+      {title: 'post 5', upvotes: 5}
+    ]
+  };
+  return o;
+}])
 .controller("MainCtrl",[
 '$scope',
-function($scope){
-  $scope.test = 'Hello World!';
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 7},
-    {title: 'post 4', upvotes: 4},
-    {title: 'post 5', upvotes: 5}
-  ];
+'posts',
+function($scope, posts){
+  $scope.posts = posts.posts;
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; };
     $scope.posts.push({
