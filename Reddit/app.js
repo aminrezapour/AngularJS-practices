d8714d4d3de1eb5f3c6edd815d6d2109984a6929
Amin Rezapour <aminrezapour@gmail.com>
2016-04-06 14:14:10 -0700

    Add posts template and route

--- Reddit/app.js
+++ Reddit/app.js
@@ -3,12 +3,17 @@ angular.module("angularReddit", ['ui.router'])
 '$stateProvider',
 '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider) {
-  
+
   $stateProvider
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
 
   $urlRouterProvider.otherwise('home');
@@ -36,12 +41,32 @@ function($scope, posts){
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upcbotes: 0}
+      ]
     });
     $scope.title = '';
     $scope.link = '';
   };
   $scope.incrementUpvotes = function(post) {
     post.upvotes += 1; // note that post is referenced automatically
+  };
+}])
+.controller("PostsCtrl", [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+  $scope.addComment = function(){
+    if ($scope.body === '') {return;}
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
   };
 }]);
