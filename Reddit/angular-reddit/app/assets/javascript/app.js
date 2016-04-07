e7b95726fb098301189b3cb141f0e31f2aa29198
Amin Rezapour <aminrezapour@gmail.com>
2016-04-06 21:49:26 -0700

    Integrating frontend with asset pipeline

--- Reddit/angular-reddit/app/assets/javascript/app.js
+++ Reddit/angular-reddit/app/assets/javascript/app.js
@@ -1,4 +1,4 @@
-angular.module("angularReddit", ['ui.router'])
+angular.module("angularReddit", ['ui.router', 'templates'])
 .config([
 '$stateProvider',
 '$urlRouterProvider',
@@ -7,66 +7,14 @@ function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: '/home',
-      templateUrl: '/home.html',
+      templateUrl: 'home/_home.html',
       controller: 'MainCtrl'
     })
     .state('posts', {
       url: '/posts/{id}',
-      templateUrl: '/posts.html',
+      templateUrl: 'posts/_posts.html',
       controller: 'PostsCtrl'
     });
 
   $urlRouterProvider.otherwise('home');
 }])
-.factory('posts', [
-function(){
-  var o = {
-    posts: [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 7},
-      {title: 'post 4', upvotes: 4},
-      {title: 'post 5', upvotes: 5}
-    ]
-  };
-  return o;
-}])
-.controller("MainCtrl",[
-'$scope',
-'posts',
-function($scope, posts){
-  $scope.posts = posts.posts;
-  $scope.addPost = function(){
-    if(!$scope.title || $scope.title === '') { return; };
-    $scope.posts.push({
-      title: $scope.title,
-      link: $scope.link,
-      upvotes: 0,
-      comments: [
-        {author: 'Joe', body: 'Cool post!', upvotes: 0},
-        {author: 'Bob', body: 'Great idea but everything is wrong!', upcbotes: 0}
-      ]
-    });
-    $scope.title = '';
-    $scope.link = '';
-  };
-  $scope.incrementUpvotes = function(post) {
-    post.upvotes += 1; // note that post is referenced automatically
-  };
-}])
-.controller("PostsCtrl", [
-'$scope',
-'$stateParams',
-'posts',
-function($scope, $stateParams, posts){
-  $scope.post = posts.posts[$stateParams.id];
-  $scope.addComment = function(){
-    if ($scope.body === '') {return;}
-    $scope.post.comments.push({
-      body: $scope.body,
-      author: 'user',
-      upvotes: 0
-    });
-    $scope.body = '';
-  };
-}]);
---
+++ Reddit/angular-reddit/app/assets/javascript/posts/posts.js
@@ -0,0 +1,14 @@
+angular.module('angularReddit')
+.factory('posts', [
+function(){
+  var o = {
+    posts: [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 7},
+      {title: 'post 4', upvotes: 4},
+      {title: 'post 5', upvotes: 5}
+    ]
+  };
+  return o;
+}])
