f4bcc0e56b7db3517e079ebabd30ff528e65afd8
Amin Rezapour <aminrezapour@gmail.com>
2016-04-08 02:40:19 -0700

    Loading posts inside index view and creating new post

--- Reddit/angular-reddit/app/assets/javascript/home/mainCtrl.js
+++ Reddit/angular-reddit/app/assets/javascript/home/mainCtrl.js
@@ -5,15 +5,10 @@ angular.module('angularReddit')
 function($scope, posts){
   $scope.posts = posts.posts;
   $scope.addPost = function(){
-    if(!$scope.title || $scope.title === '') { return; };
-    $scope.posts.push({
+    if(!$scope.title || $scope.title === '') { return; }
+    posts.create({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0,
-      comments: [
-        {author: 'Joe', body: 'Cool post!', upvotes: 0},
-        {author: 'Bob', body: 'Great idea but everything is wrong!', upcbotes: 0}
-      ]
     });
     $scope.title = '';
     $scope.link = '';
