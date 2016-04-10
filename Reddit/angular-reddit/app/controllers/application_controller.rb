c668a2e898573a10bf2a0d9a67f738fbf6ca6a25
Amin Rezapour <aminrezapour@gmail.com>
2016-04-10 00:48:50 -0700

    Add devise for user authentication

--- Reddit/angular-reddit/app/controllers/application_controller.rb
+++ Reddit/angular-reddit/app/controllers/application_controller.rb
@@ -5,8 +5,15 @@ class ApplicationController < ActionController::Base
 
   respond_to :json
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # This is a bad practice, a welcome controller is the best alternative
   def angular
     render 'layouts/application'
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 end
